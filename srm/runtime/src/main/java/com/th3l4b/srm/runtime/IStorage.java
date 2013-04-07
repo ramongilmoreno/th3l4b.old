9908262131183dc10ca71b929a6c99df3f718bd6
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2013-04-07 21:52:58 +0000

    Fix for generics.

--- srm/runtime/src/main/java/com/th3l4b/srm/runtime/IStorage.java
+++ srm/runtime/src/main/java/com/th3l4b/srm/runtime/IStorage.java
@@ -1,5 +1,5 @@
 package com.th3l4b.srm.runtime;
 
 public interface IStorage {
-	<T extends IRuntimeEntity> void commit(Iterable<T> entity) throws Exception;
+	<T extends IRuntimeEntity<?>> void commit(Iterable<T> entity) throws Exception;
 }
