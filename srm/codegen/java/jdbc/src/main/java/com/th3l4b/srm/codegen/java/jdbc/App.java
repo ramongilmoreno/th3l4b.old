c9ac0b1ee5b3082813e6e44bbcd8b22b6202d485
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2013-05-08 22:47:38 +0000

    Preparing JDBC projects. Still empty, but dependencies and svn ignores are set.

---
+++ srm/codegen/java/jdbc/src/main/java/com/th3l4b/srm/codegen/java/jdbc/App.java
@@ -0,0 +1,13 @@
+package com.th3l4b.srm.codegen.java.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
