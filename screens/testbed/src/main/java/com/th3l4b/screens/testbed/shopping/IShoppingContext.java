883d5a0afbd421afb074c02c205b9ae85b67a888
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2013-08-29 22:51:47 +0000

    This was missing in the shopping sample.

---
+++ screens/testbed/src/main/java/com/th3l4b/screens/testbed/shopping/IShoppingContext.java
@@ -0,0 +1,18 @@
+package com.th3l4b.screens.testbed.shopping;
+
+import java.util.Locale;
+
+import com.th3l4b.screens.base.utils.IScreensClientDescriptor;
+import com.th3l4b.screens.base.utils.IScreensConfiguration;
+import com.th3l4b.screens.testbed.shopping.data.IShoppingData;
+
+public interface IShoppingContext {
+	IScreensConfiguration getContext () throws Exception;
+	void setContext (IScreensConfiguration context) throws Exception;
+	IScreensClientDescriptor getClient () throws Exception;
+	void setClient (IScreensClientDescriptor client) throws Exception;
+	IShoppingData getData () throws Exception;
+	void setData (IShoppingData data) throws Exception;
+	Locale getLocale () throws Exception;
+	void setLocale (Locale locale) throws Exception;
+}
